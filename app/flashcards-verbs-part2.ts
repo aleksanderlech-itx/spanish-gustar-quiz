ed96e886cf385b5600cbfb4632e4ddfd5eb3b7e4
Aleksander Lech <aleksander.lech@itixo.com>
2026-08-21 13:06:36 +0200

    Add remaining 250 flashcard verb entries

---
+++ app/flashcards-verbs-part2.ts
@@ -0,0 +1,252 @@
+export const FLASHCARD_VERBS_PART_2 = `
+borrar|to erase; to delete
+dibujar|to draw
+pintar|to paint
+imprimir|to print
+publicar|to publish
+encender|to turn on
+apagar|to turn off
+conectar|to connect
+desconectar|to disconnect
+cargar|to load; to charge
+descargar|to download
+instalar|to install
+actualizar|to update
+reparar|to repair
+funcionar|to work; to function
+seleccionar|to select
+pulsar|to press
+clicar|to click
+navegar|to browse
+eliminar|to delete
+editar|to edit
+revisar|to review
+comprobar|to check
+medir|to measure
+calcular|to calculate
+sumar|to add
+restar|to subtract
+multiplicar|to multiply
+dividir|to divide
+ordenar|to order; to sort
+clasificar|to classify
+agrupar|to group
+mezclar|to mix
+separar|to separate
+pegar|to paste; to glue
+unir|to join
+llenar|to fill
+vaciar|to empty
+cubrir|to cover
+pausar|to pause
+reiniciar|to restart
+practicar|to practice
+memorizar|to memorize
+imaginar|to imagine
+soñar|to dream
+desear|to wish; to desire
+evitar|to avoid
+tener|to have
+convertirse|to become
+disminuir|to decrease
+desarrollar|to develop
+construir|to build
+destruir|to destroy
+fabricar|to manufacture
+alquilar|to rent
+devolver|to return; to give back
+prestar|to lend
+mandar|to send; to order
+entregar|to deliver
+invertir|to invest
+descansar|to rest
+despertar|to wake
+sentar|to seat; to sit someone down
+acostar|to put to bed
+vestirse|to get dressed
+desnudar|to undress
+desnudarse|to get undressed
+lavarse|to wash oneself
+duchar|to shower someone; to shower
+bañar|to bathe someone
+peinar|to comb
+afeitar|to shave
+maquillar|to put makeup on
+maquillarse|to put on makeup
+desayunar|to have breakfast
+almorzar|to have lunch
+cenar|to have dinner
+merendar|to have a snack
+calentar|to heat
+enfriar|to cool
+hervir|to boil
+freír|to fry
+hornear|to bake
+pelar|to peel
+echar|to throw in; to add
+secar|to dry
+planchar|to iron
+barrer|to sweep
+fregar|to scrub; to mop
+arreglar|to fix; to arrange
+reciclar|to recycle
+plantar|to plant
+regar|to water
+cuidar|to care for
+alimentar|to feed
+adoptar|to adopt
+criar|to raise; to breed
+cazar|to hunt
+pescar|to fish
+morder|to bite
+ladrar|to bark
+maullar|to meow
+fotografiar|to photograph
+grabar|to record
+filmar|to film
+entrenar|to train
+competir|to compete
+empatar|to draw; to tie
+marcar|to score; to mark
+atrapar|to catch
+golpear|to hit
+patear|to kick
+pedalear|to pedal
+esquiar|to ski
+patinar|to skate
+escalar|to climb
+despegar|to take off
+aterrizar|to land
+facturar|to invoice; to check in luggage
+embarcar|to board; to embark
+informar|to inform
+confirmar|to confirm
+avisar|to warn; to notify
+anunciar|to announce
+comunicar|to communicate
+traducir|to translate
+pronunciar|to pronounce
+deletrear|to spell
+corregir|to correct
+aprobar|to pass; to approve
+suspender|to fail; to suspend
+examinar|to examine
+evaluar|to evaluate
+completar|to complete
+rellenar|to fill in
+investigar|to investigate
+analizar|to analyze
+experimentar|to experiment
+demostrar|to demonstrate
+concluir|to conclude
+gestionar|to manage
+liderar|to lead
+colaborar|to collaborate
+participar|to participate
+asistir|to attend
+proponer|to propose
+negociar|to negotiate
+contratar|to hire
+despedir|to fire; to dismiss
+emplear|to employ
+solicitar|to request; to apply for
+contactar|to contact
+apoyar|to support
+resolver|to solve
+solucionar|to solve; to fix
+implementar|to implement
+diseñar|to design
+programar|to program
+depurar|to debug
+desplegar|to deploy
+documentar|to document
+planificar|to plan
+estimar|to estimate
+priorizar|to prioritize
+asignar|to assign
+monitorizar|to monitor
+controlar|to control
+asegurar|to ensure
+proteger|to protect
+bloquear|to block
+desbloquear|to unblock
+registrarse|to register; to sign up
+iniciar|to start; to initiate
+acceder|to access
+denegar|to deny
+recuperar|to recover
+renombrar|to rename
+sincronizar|to synchronize
+exportar|to export
+filtrar|to filter
+desmarcar|to uncheck
+avanzar|to advance
+retroceder|to go back
+ocultar|to hide
+expandir|to expand
+contraer|to collapse; to contract
+activar|to activate
+desactivar|to deactivate
+configurar|to configure
+ajustar|to adjust
+restablecer|to reset
+duplicar|to duplicate
+arrastrar|to drag
+soltar|to release; to drop
+deslizar|to swipe; to slide
+acercar|to zoom in; to bring closer
+alejar|to zoom out; to move away
+escanear|to scan
+archivar|to archive
+mencionar|to mention
+notificar|to notify
+suscribir|to subscribe
+renovar|to renew
+vencer|to expire; to defeat
+caducar|to expire
+reclamar|to complain; to claim
+reembolsar|to refund
+donar|to donate
+regalar|to give as a gift
+inspeccionar|to inspect
+pesar|to weigh
+registrar|to register; to record
+vigilar|to watch; to monitor
+denunciar|to report; to denounce
+rescatar|to rescue
+salvar|to save
+atacar|to attack
+defender|to defend
+luchar|to fight
+matar|to kill
+herir|to injure
+curar|to cure
+sanar|to heal
+diagnosticar|to diagnose
+recetar|to prescribe
+respirar|to breathe
+toser|to cough
+estornudar|to sneeze
+sangrar|to bleed
+suceder|to happen
+pertenecer|to belong
+depender|to depend
+significar|to mean
+representar|to represent
+establecer|to establish
+celebrar|to celebrate
+aprovechar|to take advantage of
+manejar|to handle; to drive
+admitir|to admit
+abandonar|to abandon
+aplicar|to apply
+causar|to cause
+conceder|to grant
+conservar|to conserve
+contener|to contain
+contribuir|to contribute
+desaparecer|to disappear
+exigir|to demand; to require
+expresar|to express
+indicar|to indicate
+`.trim();
